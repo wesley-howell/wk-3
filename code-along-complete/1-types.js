5f856a842c7e4c822fbb4fb6601420388ae7853d
wesley-howell <81998705+wesley-howell@users.noreply.github.com>
2021-04-13 08:40:51 -0500

    Initial commit

---
+++ code-along-complete/1-types.js
@@ -0,0 +1,14 @@
+// Goal: A program that stores my lunch order in the computer's memory, then writes it to the console
+// Write the algorithm, in the comments. Write the code.
+
+// Store the item I want to eat
+let food = `tacos`
+
+// Store the quantity I want
+let quantity = 3
+
+// Make an English sentence
+let sentence = `I would like ${quantity} ${food}, please.`
+
+// Write the sentence to the console
+console.log(sentence)
---
+++ code-along/1-types.js
@@ -0,0 +1,10 @@
+// Goal: A program that stores my lunch order in the computer's memory, then writes it to the console
+// Write the algorithm, in the comments. Write the code.
+
+// Store the item I want to eat
+
+// Store the quantity I want
+
+// Make an English sentence
+
+// Write the sentence to the console
